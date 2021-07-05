4beaeabbcd2e50af5fc6730616272911b0c5e6cc
joanajvc <j.v.carvalheiro@gmail.com>
2021-07-05 20:52:01 +0100

    inserted search input form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-This is going to be a dictionary app
+<h1> Dictionary</h1>
       </header>
+<Dictionary />
+      <footer className="App-footer">
+This project was coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/joana-vasconcelos-carvalheiro-569056a5/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Joana Vasconcelos
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/joanavjc/dictionary-react-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        open-sourced on GitHub
+      </a>{" "}
+      </footer>
+    </div>
     </div>
   );
 }
