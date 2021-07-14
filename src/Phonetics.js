7b70fe23c71ef7ae6921118d6abd63b8743b4f70
joanajvc <j.v.carvalheiro@gmail.com>
2021-07-14 18:57:24 +0100

    added phonetics with link to how the words are pronounced

---
+++ src/Phonetics.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Phonetics(props) {
+    return (
+        <div className="Phonetics">
+            <a href={props.phonetics.audio} target="_blank" rel="noreferrer">Listen</a>
+            <br />
+            {props.phonetics.text}
+        </div>
+    );
+}
