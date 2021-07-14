2082191d0e1335ca7df1be78f950ea8531bfe0be
joanajvc <j.v.carvalheiro@gmail.com>
2021-07-14 15:44:53 +0100

    added components that shows synonyms when they exist

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import Example from "./Example";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +10,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+             
               {index + 1}. {definition.definition}
               <br />
-              <em>{definition.example} </em>
+              <Example example={definition.example} />
+          
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
