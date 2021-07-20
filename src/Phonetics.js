98d8a14d2803f0c4913cf25d14134d6a578183f0
joanajvc <j.v.carvalheiro@gmail.com>
2021-07-20 17:12:55 +0100

    added icon for phonetics sound

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,11 +1,21 @@
 import React from "react";
+import play from "./play.svg";
+import "./Dictionary.css";
 
 export default function Phonetics(props) {
+    let audio = new Audio(props.phonetics.audio)
+
+    function playSound() {
+        audio.play();
+    }
+
     return (
-        <div className="Phonetics">
-            <a href={props.phonetics.audio} target="_blank" rel="noreferrer">Listen</a>
-            <br />
-            {props.phonetics.text}
-        </div>
+      <div className="Phonetics">
+        <span onClick={playSound}>
+          <img src={play} /> Listen
+        </span>
+        <br />
+        {props.phonetics.text}
+      </div>
     );
 }
